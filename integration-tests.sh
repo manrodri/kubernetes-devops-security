ab7e5c4469c6b995fe46634004e749a820fc8359
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-08-29 09:25:44 +0100

    loggin integration-test script

--- integration-tests.sh
+++ integration-tests.sh
@@ -11,9 +11,13 @@ echo $applicationURL:$PORT$applicationURI
 
 if [[ ! -z "$PORT" ]];
 then
-
+    echo "running ---> curl -s $applicationURL:$PORT/$applicationURI"
     response=$(curl -s $applicationURL:$PORT/$applicationURI)
+    echo "response: ${response}"
+    
+    echo "running ---> curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT/$applicationURI"
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT/$applicationURI)
+    echo "http_code: ${http_code}"
 
     if [[ "$response" == 100 ]];
         then

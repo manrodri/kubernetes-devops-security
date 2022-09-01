61bb49a735600ea91d958e8f989a39aab52659bb
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-09-01 08:31:13 +0100

    prod deployment

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -4,10 +4,10 @@
 
 sleep 30s
 
-if [[ $(kubectl -n prod rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n prod rollout undo deploy ${deploymentName}
+    kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
